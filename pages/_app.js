98d80c722582dab0636006dd4f1ee141c1c1a260
whoisarjen <Kamilow97@gmail.com>
2021-12-05 19:31:49 +0100

    Redux toolkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { store } from '../redux/store'
+import { Provider } from 'react-redux'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Navbar/>
+        <Component {...pageProps} />
+      <Footer/>
+    </Provider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
 // import styles from '../styles/Home.module.css'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
 
 export default function Home() {
   return (
-    <>
-      <Navbar/>
-      <div classname="home">
-        Home
-      </div>
-      <Footer/>
-    </>
+    <div classname="home">
+      Home
+    </div>
   )
 }
