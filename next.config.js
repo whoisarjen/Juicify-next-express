6ad25ede31e4a1f21476d8d07bb199e1d25dbb84
whoisarjen <kamilow97@gmail.com>
2021-12-11 00:16:17 +0100

    Correct PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const withPWA = require("next-pwa");
 const nextTranslate = require("next-translate");
 
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === "development"
+  },
   ...nextTranslate(),
-};
+});
