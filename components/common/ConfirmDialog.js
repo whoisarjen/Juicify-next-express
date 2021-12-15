8578bcce00e2d1ad2f7c990e3bc6641c0ab3f3e8
whoisarjen <kamilow97@gmail.com>
2021-12-15 01:10:54 +0100

    Update ConfirmDialog.js

--- components/common/ConfirmDialog.js
+++ components/common/ConfirmDialog.js
@@ -14,12 +14,11 @@ const ConfirmDialog = ({ isDialog, closeDialog, confirm }) => {
                 aria-describedby="alert-dialog-description"
             >
                 <DialogTitle id="alert-dialog-title">
-                    {"Use Google's location service?"}
+                    Confirm
                 </DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-description">
-                        Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.
+                        The action can NOT be undone.
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
