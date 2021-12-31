571328e5f824439bbef40bb4fd71aee8b9f37a93
whoisarjen <kamilow97@gmail.com>
2021-12-31 20:46:47 +0100

    v

--- components/coach/Welcome.tsx
+++ components/coach/Welcome.tsx
@@ -1,4 +1,6 @@
 import { FunctionComponent } from "react";
+import Button from '@mui/material/Button';
+import styles from '../../styles/coach.module.css'
 
 interface WelcomeProps {
     setStep: (arg0: string) => void
@@ -6,10 +8,13 @@ interface WelcomeProps {
 
 const Welcome: FunctionComponent<WelcomeProps> = ({ setStep }) => {
     return (
-        <div>
-            <div className="title">Program type</div>
-            <div className="description">Choose program type, which best fit your goal.</div>
-            <button onClick={() => setStep('Result')}>Result</button>
+        <div className={styles.welcome}>
+            <div className="title">Welcome</div>
+            <div className="description">Welcome to your coach, let's together achieve your goal! What diet should we start with?</div>
+            <Button variant="contained" onClick={() => setStep('')}>Muscle building</Button>
+            <Button variant="contained" onClick={() => setStep('')}>Recomposition</Button>
+            <Button variant="contained" onClick={() => setStep('')}>Losing weight</Button>
+            <Button variant="contained" onClick={() => setStep('')}>Custome diet</Button>
         </div>
     )
 }
--- pages/coach.tsx
+++ pages/coach.tsx
@@ -4,14 +4,6 @@ import Result from "../components/coach/Result";
 import Welcome from "../components/coach/Welcome";
 import { expectLoggedIN } from "../utils/checkAuth";
 
-const coachOptions = [
-    { link: '', title: 'Muscle build', description: 'Juicify gona prepare program, which will allow you to optimal grow muscle, while keeping fat as low as it is possible.' },
-    { link: '', title: 'Recomposition' },
-    { link: '', title: 'Lose weight' },
-    { link: '', title: 'Reverse diet' },
-    { link: '', title: 'Custome' }
-]
-
 const Coach: FunctionComponent = () => {
     expectLoggedIN()
     const [step, setStep] = useState('Standard')
