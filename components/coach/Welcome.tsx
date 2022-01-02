2a784da178cc4092ea470c61f5ef48ebd0758afd
whoisarjen <kamilow97@gmail.com>
2022-01-02 05:35:24 +0100

    Update Welcome.tsx

--- components/coach/Welcome.tsx
+++ components/coach/Welcome.tsx
@@ -10,8 +10,8 @@ const Welcome: FunctionComponent<WelcomeProps> = ({ setStep }) => {
     return (
         <div className={styles.welcome}>
         <div className={styles.AddWeightMainTitle}><div>Welcome</div></div>
-            <div className="description">Welcome to your coach, I am an Artificial Intelligence created to analize your eatting habits and help you achieve goal as quickly as it's possible! What kind of diet should we start with?</div>
-            <Button variant="contained" onClick={() => setStep('CheckingTodayData')}>Sounds cool!</Button>
+            <div className="description">Welcome to your coach, I am an Artificial Intelligence created to analize your eatting habits and help you achieve goal as quickly as it's possible!</div>
+            <Button variant="contained" onClick={() => setStep('CheckingTodayData')}>Let's start!</Button>
         </div>
     )
 }
