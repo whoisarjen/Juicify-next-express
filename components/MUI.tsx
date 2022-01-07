6c58e3b4855a2f07514caa55c465021e469ad02e
whoisarjen <kamilow97@gmail.com>
2022-01-07 12:09:10 +0100

    MUI seperated

---
+++ components/MUI.tsx
@@ -0,0 +1,49 @@
+import { createTheme, ThemeProvider as ThemeProviderMUI } from "@mui/material/styles";
+import { FunctionComponent, useState, useMemo, useEffect } from "react";
+import { ColorModeContext } from '../hooks/useDarkMode'
+
+interface MUIProps {
+    children: any
+}
+
+const MUI: FunctionComponent<MUIProps> = ({ children }) => {
+    const [mode, setMode]: any = useState('light')
+
+    const theme = createTheme({
+        typography: {
+            fontFamily: "Quicksand, sans-serif",
+        },
+        palette: {
+            mode
+        },
+    });
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+
+    useEffect(() => setMode(JSON.parse(localStorage.getItem('isDarkMode')) ? 'dark' : 'light'), [])
+
+    useEffect(() => {
+        if (mode === 'dark') {
+            document.documentElement.style.setProperty('--theme-background', '#121212')
+        } else {
+            document.documentElement.style.setProperty('--theme-background', '#ffffff')
+        }
+    }, [mode])
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProviderMUI theme={theme}>
+                {children}
+            </ThemeProviderMUI>
+        </ColorModeContext.Provider>
+    )
+}
+
+export default MUI
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,8 @@ import { store } from "../redux/store";
 import { Provider } from "react-redux";
 import Layout from "../components/Layout";
 import Socket from "../components/Socket";
-import { FunctionComponent, useState, useEffect, useMemo } from "react";
-import { createTheme, ThemeProvider as ThemeProviderMUI } from "@mui/material/styles";
-import { ColorModeContext } from '../hooks/useDarkMode'
+import { FunctionComponent } from "react";
+import MUI from "../components/MUI";
 
 interface MyAppProps {
     Component: any,
@@ -14,40 +13,16 @@ interface MyAppProps {
 }
 
 const MyApp: FunctionComponent<MyAppProps> = ({ Component, pageProps }) => {
-    const [mode, setMode]: any = useState('light')
-
-    const theme = createTheme({
-        typography: {
-            fontFamily: "Quicksand, sans-serif",
-        },
-        palette: {
-            mode
-        },
-    });
-
-    const colorMode = useMemo(
-        () => ({
-            toggleColorMode: () => {
-                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-            },
-        }),
-        [],
-    );
-
-    useEffect(() => setMode(JSON.parse(localStorage.getItem('isDarkMode')) ? 'dark' : 'light'), [])
-
     return (
-        <ColorModeContext.Provider value={colorMode}>
-            <ThemeProviderMUI theme={theme}>
-                <Provider store={store}>
-                    <Socket>
-                        <Layout>
-                            <Component {...pageProps} />
-                        </Layout>
-                    </Socket>
-                </Provider>
-            </ThemeProviderMUI>
-        </ColorModeContext.Provider>
+        <MUI>
+            <Provider store={store}>
+                <Socket>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </Socket>
+            </Provider>
+        </MUI>
     );
 }
 
