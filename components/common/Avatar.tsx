e51c48a3069be22800c754dcb0d096e3ee31af78
whoisarjen <kamilow97@gmail.com>
2022-01-10 19:34:21 +0100

    Update Avatar.tsx

--- components/common/Avatar.tsx
+++ components/common/Avatar.tsx
@@ -2,17 +2,29 @@ import Avatar from '@mui/material/Avatar';
 
 const AvatarMUI = ({ user, size = '110px' }) => {
     return (
-        <Avatar
-            sx={{ background: 'none !important', width: size, height: size, margin: 'auto' }}
-            alt={`${user.login} ${user.name} ${user.surname} on Juicify`}
-            src={`https://juicify.app:4000/server/avatar/${user._id}.jpg`}
-        >
-            <Avatar
-                sx={{ background: 'none !important', width: size, height: size, margin: 'auto' }}
-                alt={`${user.login} ${user.name} ${user.surname} on Juicify`}
-                src="http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.77a8e0c9.png&w=48&q=75"
-            />
-        </Avatar>
+        <>
+            {
+                user
+                    ?
+                    <Avatar
+                        sx={{ background: 'none !important', width: size, height: size, margin: 'auto' }}
+                        alt={`${user.login} ${user.name} ${user.surname} on Juicify`}
+                        src={`https://juicify.app:4000/server/avatar/${user._id}.jpg`}
+                    >
+                        <Avatar
+                            sx={{ background: 'none !important', width: size, height: size, margin: 'auto' }}
+                            alt={`${user.login} ${user.name} ${user.surname} on Juicify`}
+                            src='/images/logo.png'
+                        />
+                    </Avatar>
+                    :
+                    <Avatar
+                        sx={{ background: 'none !important', width: size, height: size, margin: 'auto' }}
+                        alt={`${user.login} ${user.name} ${user.surname} on Juicify`}
+                        src='/images/logo.png'
+                    />
+            }
+        </>
     )
 }
 
