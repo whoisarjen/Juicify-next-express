5d25f584973b38eec978aae71d86b372636a00f0
whoisarjen <kamilow97@gmail.com>
2022-01-12 06:54:24 +0100

    Update Spinner.tsx

--- client/components/common/Spinner.tsx
+++ client/components/common/Spinner.tsx
@@ -1,13 +1,16 @@
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 import { FunctionComponent } from "react";
+import { useTheme } from "../../hooks/useTheme";
 
 const Spinner: FunctionComponent = () => {
+    const [getTheme]: any = useTheme()
+
     return (
         <div className="spinner">
             <Loader
                 type="ThreeDots"
-                color="#1976d2"
+                color={getTheme('PRIMARY')}
                 height={100}
                 width={100}
             />
