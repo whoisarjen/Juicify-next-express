c999aae1979b23522dfa0c97933542e8f0cdb00c
whoisarjen <kamilow97@gmail.com>
2022-01-19 19:58:51 +0100

    jest without ts

---
+++ server/mongoDB/__TEXT__/product.test.ts
@@ -0,0 +1,13 @@
+test('product', () => {
+    // describe('checking product route', () => {
+        // console.log('halo')
+    // })
+})
+
+describe('get product route', () => {
+    describe('given product does not exists', () => {
+        describe('it should return an empty array', () => {
+            expect(true).toBe(true);
+        })
+    })
+})
