36a92587667d63c271cbf7e651bd5387040a7e7c
whoisarjen <kamilow97@gmail.com>
2022-01-20 22:46:57 +0100

    v

--- server/build/mongoDB/functions/handleError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (error, where) => {
-    console.log(`Got error in ${where}: ${error}`);
-    // switch (code) {
-    //     default:
-    //         return res.status(500).send({ error: 'Internal Server Error' })
-    // }
-};
