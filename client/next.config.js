0abf0aded13d46d36b63dd88fded1c284aed832f
whoisarjen <kamilow97@gmail.com>
2022-02-01 20:45:56 +0100

    v

--- client/next.config.js
+++ client/next.config.js
@@ -2,12 +2,12 @@ const withPWA = require("next-pwa");
 const nextTranslate = require("next-translate");
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  // pwa: {
-  //   dest: "public",
-  //   register: true,
-  //   skipWaiting: true,
-  //   disable: process.env.NODE_ENV === "development"
-  // },
-  ...nextTranslate(),
+    reactStrictMode: true,
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === "development"
+    },
+    ...nextTranslate(),
 });
