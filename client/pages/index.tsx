69ee4546021d13b995b6f6d1b156e1d951462683
whoisarjen <kamilow97@gmail.com>
2022-02-02 23:05:17 +0100

    Update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,10 @@
-import { useRouter } from "next/router";
 import { FunctionComponent } from "react";
 import styles from '../styles/home.module.css'
 
 const Home: FunctionComponent = () => {
-    const router: any = useRouter()
-    router.push('/login')
     return (
         <div className={styles.home}>
-            Home
-
+            Przerwa techniczna...
         </div>
     );
 }
