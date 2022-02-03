9b1b0fa4276c5aa7f5f3ecffe1474bf2825b5bcb
whoisarjen <kamilow97@gmail.com>
2022-02-03 02:33:45 +0100

    https on server

--- server/server.ts
+++ server/server.ts
@@ -6,16 +6,36 @@ import connect from './utils/connect'
 import routes from './utils/routes'
 import { socket } from './utils/socket';
 import createServer from "./utils/server";
+import * as fs from 'fs';
+import * as https from 'https';
 
 const app = createServer();
 
-const server = app.listen(process.env.PORT, async () => {
-    logger.info(`Listening on port ${process.env.PORT} (http://localhost:${process.env.PORT})`);
-    app.set('socket', io);
-    await connect();
-    socket({ io })
-    routes(app);
-});
+console.log(`Server is working with ${process.env.NODE_ENV}`)
+let server;
+
+if (process.env.NODE_ENV == "development") {
+    server = app.listen(process.env.PORT, async () => {
+        logger.info(`Listening on port ${process.env.PORT} (http://localhost:${process.env.PORT})`);
+        app.set('socket', io);
+        await connect();
+        socket({ io })
+        routes(app);
+    });
+}
+
+if (process.env.NODE_ENV == "production") {
+    server = https.createServer({
+        key: fs.readFileSync('./../../../../etc/letsencrypt/live/juicify.app/privkey.pem'),
+        cert: fs.readFileSync('./../../../../etc/letsencrypt/live/juicify.app/fullchain.pem')
+    }, app).listen(process.env.PORT, async () => {
+        logger.info(`Listening on port ${process.env.PORT} (http://localhost:${process.env.PORT})`);
+        app.set('socket', io);
+        await connect();
+        socket({ io })
+        routes(app);
+    })
+}
 
 const io = new Server(server, {
     cors: {
