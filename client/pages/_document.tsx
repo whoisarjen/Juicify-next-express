e40161c43f1ea6909b394b81359d98473ce50678
whoisarjen <kamilow97@gmail.com>
2022-02-04 15:37:54 +0100

    v

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -23,14 +23,21 @@ class MyDocument extends Document {
                     <meta name='msapplication-tap-highlight' content='no' />
                     <meta name='theme-color' content='#121212' />
 
-                    {/* <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
-                    <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
-                    <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
-                    <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
+                    <link rel="apple-touch-icon" sizes="57x57" href="icons/apple-icon-57x57.png" />
+                    <link rel="apple-touch-icon" sizes="60x60" href="icons/apple-icon-60x60.png" />
+                    <link rel="apple-touch-icon" sizes="72x72" href="icons/apple-icon-72x72.png" />
+                    <link rel="apple-touch-icon" sizes="76x76" href="icons/apple-icon-76x76.png" />
+                    <link rel="apple-touch-icon" sizes="114x114" href="icons/apple-icon-114x114.png" />
+                    <link rel="apple-touch-icon" sizes="120x120" href="icons/apple-icon-120x120.png" />
+                    <link rel="apple-touch-icon" sizes="144x144" href="icons/apple-icon-144x144.png" />
+                    <link rel="apple-touch-icon" sizes="152x152" href="icons/apple-icon-152x152.png" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-icon-180x180.png" />
+                    <link rel="icon" type="image/png" sizes="192x192" href="icons/android-icon-192x192.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="96x96" href="icons/favicon-96x96.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
 
-                    <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-                    <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-                    <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' /> */}
+                    {/* <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' /> */}
 
                     <meta name='twitter:card' content='summary' />
                     <meta name='twitter:url' content='https://juicify.app' />
@@ -46,7 +53,6 @@ class MyDocument extends Document {
                     <meta property='og:image' content='https://juicify.app/icons/apple-touch-icon.png' />
 
                     {/* apple splash screen images */}
-                    <link rel="apple-touch-icon" href="icons/apple-icon-180.png" />
                     <meta name="apple-mobile-web-app-capable" content="yes" />
                     <link rel="apple-touch-startup-image" href="icons/apple-splash-2048-2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
                     <link rel="apple-touch-startup-image" href="icons/apple-splash-2732-2048.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
