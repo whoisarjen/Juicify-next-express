1671eb96a0460396d472d5ec38d7eb87d48ebe87
whoisarjen <kamilow97@gmail.com>
2022-02-04 17:21:51 +0100

    v

--- client/components/layout/Footer.tsx
+++ client/components/layout/Footer.tsx
@@ -13,42 +13,52 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div>©2022 Juicify.app</div>
-      {token.login && (
-        <nav className="footerMenu">
-          <Link passHref href="/coach/">
-            <a className="footerMenuElement">
-              <EmojiEventsIcon color="primary" />
-            </a>
-          </Link>
-          <Link passHref href="/workout">
-            <div className="footerMenuElement">
-              <FitnessCenterIcon color="primary" />
-            </div>
-          </Link>
-          <Link passHref href="/barcode">
-            <div className="footerMenuElement">
-              <PhotoCameraIcon color="primary" />
-            </div>
-          </Link>
-          <Link passHref href={`/${token.login}/nutrition-diary/${getShortDate()}`}>
-            <a className="footerMenuElement">
-              <BookIcon color="primary" />
-            </a>
-          </Link>
-          <Link passHref href={`/${token.login}`}>
-            <a className="footerMenuElement">
-              <Avatar
-                sx={{ width: '28px', height: '28px' }}
-                alt={`${token.login} ${token.name} ${token.surname} on Juicify`}
-                src={`https://juicify.app:4000/server/avatar/${token._id}.jpg`}
-              >
-                <AccountCircleIcon color="primary" />
-              </Avatar>
-            </a>
-          </Link>
-        </nav>
-      )}
+      {
+        token.login
+          ?
+          (
+            <>
+              <div />
+              <nav className="footerMenu">
+                <Link passHref href="/coach/">
+                  <a className="footerMenuElement">
+                    <EmojiEventsIcon color="primary" />
+                  </a>
+                </Link>
+                <Link passHref href="/workout">
+                  <div className="footerMenuElement">
+                    <FitnessCenterIcon color="primary" />
+                  </div>
+                </Link>
+                <Link passHref href="/barcode">
+                  <div className="footerMenuElement">
+                    <PhotoCameraIcon color="primary" />
+                  </div>
+                </Link>
+                <Link passHref href={`/${token.login}/nutrition-diary/${getShortDate()}`}>
+                  <a className="footerMenuElement">
+                    <BookIcon color="primary" />
+                  </a>
+                </Link>
+                <Link passHref href={`/${token.login}`}>
+                  <a className="footerMenuElement">
+                    <Avatar
+                      sx={{ width: '28px', height: '28px' }}
+                      alt={`${token.login} ${token.name} ${token.surname} on Juicify`}
+                      src={`https://juicify.app:4000/server/avatar/${token._id}.jpg`}
+                    >
+                      <AccountCircleIcon color="primary" />
+                    </Avatar>
+                  </a>
+                </Link>
+              </nav>
+            </>
+          )
+          :
+          (
+            <div>©2022 Juicify.app</div>
+          )
+      }
     </footer>
   );
 };
