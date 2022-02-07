38dd77612dba5070af582bea461156b67b88d5ce
whoisarjen <kamilow97@gmail.com>
2022-02-07 16:58:16 +0100

    barcode v1

--- client/pages/barcode.tsx
+++ client/pages/barcode.tsx
@@ -1,21 +1,21 @@
 import { FunctionComponent } from "react";
-// import React from 'react';
-// import BarcodeScannerComponent from "react-webcam-barcode-scanner";
+import React from 'react';
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
 
 const Barcode: FunctionComponent = () => {
-    // const [data, setData] = React.useState('Not Found');
+    const [data, setData] = React.useState('Not Found');
 
     return (
         <div className="barcode">
-            {/* <BarcodeScannerComponent
-                width={500}
-                height={500}
+            <BarcodeScannerComponent
+                width={726}
+                height={408}
                 onUpdate={(err, result: any) => {
                     if (result) setData(result.text)
                     else setData('Not Found')
                 }}
             />
-            <p>{data}</p> */}
+            <p>{data}</p>
         </div>
     );
 };
