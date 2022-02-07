e2e1b2b04f7f08b980cf513519b948dff01391a6
whoisarjen <kamilow97@gmail.com>
2022-02-07 06:29:32 +0100

    fix guest daily on server site

--- server/mongoDB/controller/dailyMeasurements.controller.ts
+++ server/mongoDB/controller/dailyMeasurements.controller.ts
@@ -6,11 +6,11 @@ import { DailyMeasurementProps } from '../models/dailyMeasurement.model'
 
 export const getGuestDailyMeasurementsHandler = async (req: Request, res: Response) => {
     try {
-        if ((new Date(req.body.uniqueKey).toString() === 'Invalid Date')) {
-            logger.error(`getDailyMeasurementHandler blocked ${req.body.uniqueKey}. Invalid Date`)
+        if ((new Date(req.body.find).toString() === 'Invalid Date')) {
+            logger.error(`getDailyMeasurementHandler blocked ${req.body.find}. Invalid Date`)
             return res.status(errorBook['DATE IS REQUIRED']['CODE']).send(errorBook['DATE IS REQUIRED']['VALUE'])
         }
-        const dailyMeasurements: Array<DailyMeasurementProps> = await getDailyMeasurements(res.locals.user, req.body.uniqueKey)
+        const dailyMeasurements: Array<DailyMeasurementProps> = await getDailyMeasurements(res.locals.user, req.body.find)
         return res.send({ data: dailyMeasurements, user: res.locals.user });
     } catch (error: any) {
         logger.error(error)
