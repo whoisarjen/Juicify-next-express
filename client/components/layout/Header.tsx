156a4142f91920f943be223165cc1e36c471f6bc
whoisarjen <kamilow97@gmail.com>
2022-02-10 20:10:39 +0100

    Google analytics

--- client/components/layout/Header.tsx
+++ client/components/layout/Header.tsx
@@ -12,6 +12,17 @@ const Header: FunctionComponent<HeaderProps> = ({ title, description }) => {
             <title>{title}</title>
             <meta name='viewport' content='initial-scale=1.0, width=device-width' />
             <meta name="description" content={description} />
+            {/* Global site tag (gtag.js) - Google Analytics */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-RTCFC3JGV2"></script>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-RTCFC3JGV2');`
+                }}
+            />
         </Head>
     )
 }
