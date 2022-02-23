0d3dd09178786c9f4a5cab4c6191e320d00baccf
whoisarjen <kamilow97@gmail.com>
2022-02-23 01:11:17 +0100

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,23 @@
 const withPWA = require("next-pwa");
 const nextTranslate = require("next-translate");
 
+const getCookie = (cookieName) => {
+    let cookie = {};
+    document.cookie.split(';').forEach(function (el) {
+        let [key, value] = el.split('=');
+        cookie[key.trim()] = value;
+    })
+    return cookie[cookieName];
+}
+
 module.exports = withPWA({
     reactStrictMode: true,
     pwa: {
         dest: "public",
         register: true,
         skipWaiting: true,
+        scope: '/',
+        scope: getCookie('NEXT_LOCALE') == 'en' ? '/' : `/${getCookie('NEXT_LOCALE')}`,
         disable: process.env.NODE_ENV === 'development',
     },
     ...nextTranslate(),
