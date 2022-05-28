ac5ef25c60d94d88415bb44bbae457dade9c47ef
whoisarjen <Kamilow97@gmail.com>
2022-05-28 13:38:00 +0200

    modified:   nest/.gitignore deleted:    nest/README.md modified:   nest/package-lock.json modified:   nest/package.json modified:   nest/src/app.module.ts modified:   nest/src/main.ts new file:   nest/src/products/dto/create-product.input.ts new file:   nest/src/products/dto/update-product.input.ts new file:   nest/src/products/entities/product.entity.ts new file:   nest/src/products/products.module.ts new file:   nest/src/products/products.resolver.spec.ts new file:   nest/src/products/products.resolver.ts new file:   nest/src/products/products.service.spec.ts new file:   nest/src/products/products.service.ts new file:   nest/src/schema.gql

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: true,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    MongooseModule.forRoot(
+      'mongodb://Arjen:Preetini49e89d5b@86.106.91.210:27017/test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
+    ),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
